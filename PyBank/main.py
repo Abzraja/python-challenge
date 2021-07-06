1d1c5ed269b69832be344bfbdd066f36476723e3
Abdurrahman Raja <abdurrahman_raja@hotmail.com>
2021-07-06 22:32:23 +0100

    read current row value and next row value and added them together and printed to test

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,13 +17,25 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     
     row_count=0
-    net_value=0
+    total_net_value=0
 
     for row in csvreader:
         row_count += 1
-        net_value = net_value + int(row[1])
-    
+        total_net_value = total_net_value + int(row[1])
+        
+        #read current row value from profit/loss column
+        current_value = int(row[1])
+        #read next line value as intger (I got this code from - https://docs.python.org/3/library/csv.html)
+        next_value = int(next(csvreader)[1])
+
+        #add next line value from current line value and store in change variable
+        change = current_value + next_value
+
+
     print(f"Total Months: {row_count}")
-    print(f"Total $: {net_value}")
+    print(f"Total $: {total_net_value}")
+    print(f"Current Value: {current_value}")
+    print(f"Next Value: {next_value}")
+    print(f"Change: {change}")
     
 
