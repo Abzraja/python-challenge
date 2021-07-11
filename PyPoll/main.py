d68c98beb3efebdb4670b65ed92fd64c26b0a7c1
Abdurrahman Raja <abdurrahman_raja@hotmail.com>
2021-07-11 11:14:29 +0100

    PyPoll - imported os and csv modules and defined csvpath

---
+++ PyPoll/main.py
@@ -0,0 +1,13 @@
+#import module for correcting file paths across different operating systems
+import os
+
+#import module that allows us to read csv files
+import csv
+
+#import statistics module and mean function - got from (https://www.guru99.com/find-average-list-python.html)
+from statistics import mean
+
+#reference path to CSV data file
+csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+
+
