54ff31a58aa3d2a415e9741634cd9076292077e8
Abdurrahman Raja <abdurrahman_raja@hotmail.com>
2021-07-11 21:59:51 +0100

    COMPLETED FINAL VERSIONS - had a nightmare getting the format correct and issue with loop appening list twice, finally solved it

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,7 +22,6 @@ with open(csvpath) as csvfile:
     #total_vote_count to count the number of rows
     total_vote_count = 0
     candidate_votes_dict = {}
-
   # iterate through the rows
     for row in csvreader:
         
@@ -40,21 +39,7 @@ with open(csvpath) as csvfile:
 
 #print output to terminal
 
-def candidate_results():
 
-    #iterate through each key (candidate) in candidate_vote dictionary
-    for key, value in candidate_votes_dict.items():
-            #calculate the vote percentage for each key(candidate) by taking the value of the key and dividing by total_vote_count
-            candidate_percent = value / total_vote_count * 100
-            #round the candidate_percent value to 2 decimal places
-            candidate_percent = round(candidate_percent,2)
-            #show 3 figures after decimal as in the example on GitLab
-            candidate_percent = "{:.3f}".format(candidate_percent)
-            #for each key print the key, candidate_percent and value
-            print(f"{key}: {candidate_percent}% ({value})")
-
-#find max value in dictionary and return corresponding key. the max value is the greatest number of votes, therefore the key is the winner
-winner = max(candidate_votes_dict, key=candidate_votes_dict.get)
 
 def election_results_report_header():
     return(f"Election Results\n" +
@@ -62,15 +47,29 @@ def election_results_report_header():
         f"Total Votes: {total_vote_count}\n" +
         "-------------------------")
 
+def candidate_results():
+    results_list = []
+    #iterate through each key (candidate) in candidate_vote dictionary
+    for key, value in candidate_votes_dict.items():
+        #calculate the vote percentage for each key(candidate) by taking the value of the key and dividing by total_vote_count
+        candidate_percent = value / total_vote_count * 100
+            #round the candidate_percent value to 2 decimal places
+        candidate_percent = round(candidate_percent,2)
+        #show 3 figures after decimal as in the example on GitLab
+        candidate_percent = "{:.3f}".format(candidate_percent)
+        #for each key save the key, candidate_percent and value into a list
+        results_list.append(f"{key}: {candidate_percent}% ({value})")
+    #return each item in the list on a new line        
+    return("\n".join(results_list))
+    
+    
     
 def election_results_winner():
+        #find max value in dictionary and return corresponding key. the max value is the greatest number of votes, therefore the key is the winner
+        winner = max(candidate_votes_dict, key=candidate_votes_dict.get)
         return("-------------------------\n" +
         f"Winner: {winner}\n"
         "-------------------------")
-
-print(election_results_report_header())
-candidate_results()
-print(election_results_winner())
 
 
 #output to text file
@@ -81,4 +80,19 @@ text_file_path = os.path.join(".", "analysis", "election_results.txt")
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(text_file_path, 'w') as f:
     f.write(election_results_report_header())
+    f.write("\n")
+    f.write(candidate_results())
+    f.write("\n")
     f.write(election_results_winner())
+
+
+
+
+#print to terminal - the header which contains the total vote count
+print(election_results_report_header())
+#print to terminal - the candidate_results function which contains separated list of each candidate's results
+print(candidate_results())
+#print to terminal - the winner function which contains the winner
+print(election_results_winner())
+
+
