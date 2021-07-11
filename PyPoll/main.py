8b6a48ae9364a82ff8eb9e738b8402afba3d355b
Abdurrahman Raja <abdurrahman_raja@hotmail.com>
2021-07-11 11:17:21 +0100

    PyPoll - read csv file and skipped header

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,3 +11,10 @@ from statistics import mean
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 
 
+#Read CSV file using the imported CSV module
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
